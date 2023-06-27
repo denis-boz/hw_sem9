7fe6654e2b5538a090f16754300f4248bc8313f5
Denis <denis-boz@hotmail.com>
2023-06-27 22:58:34 +0300

    Решение задачи 64

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет
+все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string GetStringOfNumbers(int start, int end)
+{
+    // Базовый случай - выход
+    if (start == end) return start.ToString();
+    // Рекурсивный случай
+    return start + ", " + GetStringOfNumbers(start - 1, end);
+
+}
+Console.WriteLine(GetStringOfNumbers(N, 1));
