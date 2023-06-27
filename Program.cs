9884d87134b5a4ac66b93e046546fdd7a3441633
Denis <denis-boz@hotmail.com>
2023-06-27 23:46:32 +0300

    Решение задачи 68

--- Program.cs
+++ Program.cs
@@ -27,6 +27,7 @@ M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
 
+/*
 Console.Write("Введите число N: ");
 int M = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число M: ");
@@ -46,4 +47,30 @@ int GetSumOfNumbers(int M, int N)
         res = M + GetSumOfNumbers(M, N);
         return res;
     }
+}
+*/
+
+/*
+Задача 68: Напишите программу вычисления функции Аккермана с
+помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+GetAkkermanFunction(m,n);
+void GetAkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0 && m > 0) return Akkerman(m - 1, 1);
+    else return (Akkerman(m - 1, Akkerman(m, n - 1)));
 }
